ccd4f97469252a011924fae1c85e8f37ac6053fd
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-02-06 20:23:57 -0600

    rename dirs

--- aws-lambda/lambda_handler.rb
+++ aws-lambda/lambda_handler.rb
@@ -1,16 +1,18 @@
-require "logger"
+require 'logger'
 require 'json'
 
 require_relative 'users/entry_point'
-require_relative 'utils/utils'
+require_relative 'helpers/args_entry'
 
 @log = Logger.new(STDOUT)
 @log.debug("Start...")
 
 def main
     args = get_params()
+    event = args[0]
+    context = nil
     {
-        "create_users" => lambda  { create_users_handler(event=args[0]) }
+        "create_users" => lambda { create_users_handler(event=event, context=context) },
     }["#{args[1]}"].call
 end
 
--- aws-lambda/users/entry_point.rb
+++ aws-lambda/users/entry_point.rb
@@ -1,6 +1,6 @@
 require_relative 'application/create_user'
 
-def create_users_handler(event)
+def create_users_handler(event, context)
     user = CreateUser.new()
     user.run(event)
 end
