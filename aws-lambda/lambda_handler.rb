28883b3fa74eb214cfcedee97b1c229733b54959
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-02-06 21:26:55 -0600

    change invoke name

--- aws-lambda/lambda_handler.rb
+++ aws-lambda/lambda_handler.rb
@@ -12,8 +12,10 @@ def main
     funtion_name = args[1]
     execution_event = {'event' => args[0], 'context' => nil}
     {
-        "create_users" => lambda { create_users_handler(execution_event=execution_event) },
-    }["#{funtion_name}"].call
+        "create_users" => (
+            lambda {create_users_handler(execution_event=execution_event)}
+        ),
+    }[funtion_name].call
 end
 
 puts main()
