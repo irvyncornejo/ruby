8eda0e4130b4030f44149f993927e4d320cfdef7
Irvyn Cornejo <irvyncornejo@gmail.com>
2023-02-06 21:10:35 -0600

    change event declaration

--- aws-lambda/lambda_handler.rb
+++ aws-lambda/lambda_handler.rb
@@ -9,11 +9,11 @@ require_relative 'helpers/args_entry'
 
 def main
     args = get_params()
-    event = args[0]
-    context = nil
+    funtion_name = args[1]
+    execution_event = {'event' => args[0], 'context' => nil}
     {
-        "create_users" => lambda { create_users_handler(event=event, context=context) },
-    }["#{args[1]}"].call
+        "create_users" => lambda { create_users_handler(execution_event=execution_event) },
+    }["#{funtion_name}"].call
 end
 
 puts main()
--- aws-lambda/users/entry_point.rb
+++ aws-lambda/users/entry_point.rb
@@ -1,6 +1,6 @@
 require_relative 'application/create_user'
 
-def create_users_handler(event, context)
+def create_users_handler(execution_event)
     user = CreateUser.new()
-    user.run(event)
+    user.run(execution_event['event'])
 end
